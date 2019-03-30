e3755ea456c2cfe93cf7ccd30cf85c3c395bd027
Dishant100 <37150427+Dishant100@users.noreply.github.com>
2019-03-30 20:11:15 +0530

    update

--- Video_recog_new/OpenCV-Face-Recognition-Python.py
+++ Video_recog_new/OpenCV-Face-Recognition-Python.py
@@ -36,7 +36,6 @@ def prepare_training_data(data_folder_path):
         if not dir_name.startswith("s"):
             continue;
             
-        label
         label = int(dir_name.replace("s", ""))
         
         subject_dir_path = data_folder_path + "/" + dir_name
@@ -53,8 +52,8 @@ def prepare_training_data(data_folder_path):
 
             image = cv2.imread(image_path)
             
-            #cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
-            #cv2.waitKey(100)
+            cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
+            cv2.waitKey(2)
             
             face, rect = detect_face(image)
             
@@ -69,6 +68,15 @@ def prepare_training_data(data_folder_path):
     
     return faces, labels
 
+def draw_rectangle(img, rect):
+    (x, y, w, h) = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
+
 
 print("Preparing data...")
 faces, labels = prepare_training_data("training-data")
@@ -78,45 +86,34 @@ print("Total faces: ", len(faces))
 print("Total labels: ", len(labels))
 
  
+
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-
 face_recognizer.train(faces, np.array(labels))
 
-
-def draw_rectangle(img, rect):
-    (x, y, w, h) = rect
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-def draw_text(img, text, x, y):
-    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
 
 print("Predicting images...")
 
+
+
 video_capture = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
 
+
+
 while True:
-    # Capture frame-by-frame
     ret, frame = video_capture.read()
 
     img = frame.copy()
-    #detect face from the image
     face, rect = detect_face(img)
 
     if face is not None:
-        #predict the image using our face recognizer 
         label, confidence = face_recognizer.predict(face)
-        #get name of respective label returned by face recognizer
-        label_text = subjects[label]
+        label_text = subjects[label]+" "+str(round(confidence,2))
         
-        #draw a rectangle around face detected
         draw_rectangle(img, rect)
-        #draw name of predicted person
         draw_text(img, label_text, rect[0], rect[1]-5)
 
-    # Display the resulting frame
     cv2.imshow('Video', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
