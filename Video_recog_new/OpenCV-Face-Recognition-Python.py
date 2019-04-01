94c374e9733307da7983bb1993e4fc23c8edc3e1
Samreen <samreenreyaz@outlook.com>
2019-04-02 02:58:37 +0530

    Minor changes

--- Video_recog_new/OpenCV-Face-Recognition-Python.py
+++ Video_recog_new/OpenCV-Face-Recognition-Python.py
@@ -1,61 +1,43 @@
 import cv2
 import os
-
 import numpy as np
 
 subjects = [""]
+face_cascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
 
 def detect_face(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    face_cascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
-
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
-    
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=8);
     if (len(faces) == 0):
         return None, None
    
     (x, y, w, h) = faces[0]
-    
     return gray[y:y+w, x:x+h], faces[0]
 
 def prepare_training_data(data_folder_path):
-    
     dirs = os.listdir(data_folder_path)
-    
     faces = []
     labels = []
     a=0
     
     for dir_name in dirs:
-
         label = a+1
         a+=1
-            
         subject_dir_path = data_folder_path + "/" + dir_name
-        
         subject_images_names = os.listdir(subject_dir_path)
-
         subjects.append(dir_name)
-        
         for image_name in subject_images_names:
-            
             image_path = subject_dir_path + "/" + image_name
-
             image = cv2.imread(image_path)
-            
-            cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
-            cv2.waitKey(2)
-            
+            #cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
+            #cv2.waitKey(2)
             face, rect = detect_face(image)
-            
         
             if face is not None:
                 faces.append(face)
                 labels.append(label)
             
     cv2.destroyAllWindows()
-    
     return faces, labels
 
 def draw_rectangle(img, rect):
@@ -64,8 +46,6 @@ def draw_rectangle(img, rect):
 
 def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-
-
 
 
 print("Preparing data...")
@@ -94,7 +74,6 @@ if face is not None:
 
 while True:
     ret, frame = video_capture.read()
-
     face, rect = detect_face(frame)
 
     if face is not None:
@@ -109,10 +88,11 @@ while True:
         else:
             confirmation.clear()
         
-        draw_rectangle(frame, rect)
-        draw_text(frame, label_text, rect[0], rect[1]-5)
+        if confidence<=50:
+            draw_rectangle(frame, rect)
+            draw_text(frame, label_text, rect[0], rect[1]-5)
 
-    cv2.imshow('Video', frame)
+    cv2.imshow('Attendance Using Facial Recognition', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
