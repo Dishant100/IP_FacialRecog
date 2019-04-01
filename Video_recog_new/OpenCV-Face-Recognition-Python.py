33a2fbebadb05f8f551783ba42e3383e340a7b3d
Dishant100 <37150427+Dishant100@users.noreply.github.com>
2019-04-01 13:00:11 +0530

    Update OpenCV-Face-Recognition-Python.py

--- Video_recog_new/OpenCV-Face-Recognition-Python.py
+++ Video_recog_new/OpenCV-Face-Recognition-Python.py
@@ -92,7 +92,7 @@ print("Predicting images...")
 
 
 video_capture = cv2.VideoCapture(0)
-faceCascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
 
 
 
