368ca7dcc8ca2b3ab5cc849f69215c99de1b45f7
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-04-02 07:46:04 +0530

    made the outside part main()

--- Video_recog_new/OpenCV-Face-Recognition-Python.py
+++ Video_recog_new/OpenCV-Face-Recognition-Python.py
@@ -65,57 +65,58 @@ def draw_rectangle(img, rect):
 def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
+def main():
+    print("Preparing data...")
+    faces, labels = prepare_training_data("training-data")
+    print("Data prepared")
 
+    print("Total faces: ", len(faces))
+    print("Total labels: ", len(labels))
 
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer.train(faces, np.array(labels))
 
-print("Preparing data...")
-faces, labels = prepare_training_data("training-data")
-print("Data prepared")
+    print("Predicting images...")
 
-print("Total faces: ", len(faces))
-print("Total labels: ", len(labels))
+    video_capture = cv2.VideoCapture(0)
+    faceCascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
 
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.train(faces, np.array(labels))
-
-print("Predicting images...")
-
-video_capture = cv2.VideoCapture(0)
-faceCascade = cv2.CascadeClassifier('opencv-files/haarcascade_frontalface_default.xml')
-
-ret, frame = video_capture.read()
-img = frame.copy()
-face, rect = detect_face(img)
-confirmation = []
-if face is not None:
-    label, confidence = face_recognizer.predict(face)
-    confirmation.append(subjects[label])
-
-
-while True:
     ret, frame = video_capture.read()
-
-    face, rect = detect_face(frame)
-
+    img = frame.copy()
+    face, rect = detect_face(img)
+    confirmation = []
     if face is not None:
         label, confidence = face_recognizer.predict(face)
-        label_text = subjects[label]+" "+str(round(confidence,2))
         confirmation.append(subjects[label])
 
-        if confirmation[0]==confirmation[-1]:
-            if len(confirmation)==50:
-                print(confirmation[0]+' is Present')
+
+    while True:
+        ret, frame = video_capture.read()
+
+        face, rect = detect_face(frame)
+
+        if face is not None:
+            label, confidence = face_recognizer.predict(face)
+            label_text = subjects[label]+" "+str(round(confidence,2))
+            confirmation.append(subjects[label])
+
+            if confirmation[0]==confirmation[-1]:
+                if len(confirmation)==50:
+                    print(confirmation[0]+' is Present')
+                    confirmation.clear()
+            else:
                 confirmation.clear()
-        else:
-            confirmation.clear()
-        
-        draw_rectangle(frame, rect)
-        draw_text(frame, label_text, rect[0], rect[1]-5)
+            
+            draw_rectangle(frame, rect)
+            draw_text(frame, label_text, rect[0], rect[1]-5)
 
-    cv2.imshow('Video', frame)
+        cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-print("Prediction complete")
+    print("Prediction complete")
+
+if __name__ == '__main__':
+    main()
